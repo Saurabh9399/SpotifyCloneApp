3a1e7e1b78d89c5d000a1d42f668d403b4fcdae4
Saurabh <saurabhj8087445@gmail.com>
2020-12-10 21:57:17 +0530

    Adding sidebarOption file and other

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="footer">
+      <h1>I am Footer</h1>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
